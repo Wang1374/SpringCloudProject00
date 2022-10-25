ff4f449b98025764747ce1c76acdc7025a64c9ba
Wang1374 <emmawang1374@gmail.com>
2022-10-24 21:49:28 -0400

    10.24 initial set up

--- gulimall-coupon/src/main/java/com/gulimall/coupon/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/gulimall/coupon/coupon/GulimallCouponApplication.java
@@ -3,8 +3,10 @@ package com.gulimall.coupon.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.gulimall.coupon.coupon.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallCouponApplication {
 
--- gulimall-member/src/main/java/com/gulimall/member/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/gulimall/member/member/GulimallMemberApplication.java
@@ -1,8 +1,12 @@
-package com.gulimall.common;
+package com.gulimall.member.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@MapperScan("com.gulimall.member.member.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallMemberApplication {
 
